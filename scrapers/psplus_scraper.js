cbbf31c7dbca12a8b6756199dbfba6c8d4f5190d
Mark Burns <77061384+MarkTheSharky@users.noreply.github.com>
2022-02-16 21:47:00 +0000

    created POST embed object for discord

--- scrapers/psplus_scraper.js
+++ scrapers/psplus_scraper.js
@@ -37,3 +37,24 @@ async function scrapeData(lastPost) {
 // scrapeData();
 
 exports.scrapeData = scrapeData
+
+
+// EMBED POST TEMPLATE
+//
+// {
+//   "username": "Playstation Plus",
+//   "embeds": [
+//     {
+//       "title": "Free Playstation Plus Games",
+//       "url": "https://blog.playstation.com/2022/01/26/playstation-plus-games-for-february-ea-sports-ufc-4-planet-coaster-console-edition-tiny-tinas-assault-on-dragon-keep-a-wonderlands-one-shot-adventure/",
+//       "description": "PlayStation Plus games for February: EA Sports UFC 4, Planet Coaster: Console Edition, Tiny Tina’s Assault on Dragon Keep: A Wonderlands One-shot Adventure",
+//       "color": 15258703,
+//       "thumbnail": {
+//         "url": "https://gmedia.playstation.com/is/image/SIEPDC/ps-plus-blue-bag-icon-01-22sep20?$200px--t$"
+//       },
+//       "image": {
+//         "url": "https://blog.playstation.com/tachyon/2022/01/5a1b94ee8548fa3391124c09f9eab6dc6838b8ef.jpg?resize=1088%2C612&crop_strategy=smart"
+//       }
+//     }
+//   ]
+// }
