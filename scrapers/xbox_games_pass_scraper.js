98acabfd1b64f9a9f437b7b2c6839e701ccb34a5
Mark Burns <77061384+MarkTheSharky@users.noreply.github.com>
2022-02-24 14:01:00 +0000

    added url for a discord test channel

--- scrapers/xbox_games_pass_scraper.js
+++ scrapers/xbox_games_pass_scraper.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-const { discordWebhook } = require('../config.json')
+const { discordWebhookTEST } = require('../config.json')
 
 
 // URL of the page we want to scrape
@@ -58,7 +58,7 @@ async function scrapeData(lastURL) {
             'Content-Type': 'application/json',
         },
         data: JSON.stringify(obj),
-        url: discordWebhook,
+        url: discordWebhookTEST,
       });
       return post
     }
